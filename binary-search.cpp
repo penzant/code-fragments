a5d0614432e9839e2e48eeb7beb61b928fde1c03
penzant <info@penzant.net>
2022-04-03 15:47:16 +0900

    Use lambda functions

--- binary-search.cpp
+++ binary-search.cpp
@@ -1,21 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool comp_binary_search(int v, int k) {
-    if (v == k) return true;
-    else return false;
-}
-
-bool comp_lower_bound(int v, int k) {
-    if (v >= k) return true;
-    else return false;
-}
-
-bool comp_upper_bound(int v, int k) {
-    if (v > k) return true;
-    else return false;
-}
-
 int binary_search(vector<int> ary, int k, bool (*comp)(int, int)) {
     int left = -1, right = ary.size();
     while (right - left > 1) {
@@ -29,14 +14,15 @@ int binary_search(vector<int> ary, int k, bool (*comp)(int, int)) {
 }
 
 int main() {
-    vector<int> test_array{1,2,5,5,7,10,22};
+    vector<int> test_array {1, 2, 5, 5, 7, 10, 22};
     cout << "array = 1, 2, 5, 5, 7, 10, 22" << endl;
-    vector<pair<string, bool (*)(int, int)> > methods{
-        make_pair("lower_bound", &comp_lower_bound),
-        make_pair("upper_bound", &comp_upper_bound),
-        make_pair("binary_search", &comp_binary_search),
+    vector<pair<string, bool (*)(int, int)> > methods {
+        make_pair("binary_search", [](int v, int k) { return v > k; }),
+        make_pair("lower_bound", [](int v, int k) { return v == k; }),
+        make_pair("upper_bound", [](int v, int k) { return v >= k; }),
     };
     for (auto p: methods) {
+        cout << endl;
         for (int key : {5, 6, 7, 10, 12}) {
             cout << p.first  << " for " << key << " is index = "
                  << binary_search(test_array, key, p.second) << endl;
