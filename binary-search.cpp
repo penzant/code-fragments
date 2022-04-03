d4ac5a812cfa5e4041375f36a296632deb485d76
penzant <info@penzant.net>
2022-04-03 16:03:06 +0900

    Refactor

--- binary-search.cpp
+++ binary-search.cpp
@@ -2,30 +2,27 @@
 using namespace std;
 
 int binary_search(vector<int> ary, int k, bool (*comp)(int, int)) {
+    // left is the max position that doesn't satisfy the condition
+    // right is the min position that satisfies the condition
     int left = -1, right = ary.size();
     while (right - left > 1) {
         int mid = left + (right - left) / 2;
         if (comp(ary[mid], k)) right = mid;
         else left = mid;
     }
-    // left is the max position that doesn't satisfy the condition
-    // right is the min position that satisfies the condition
     return right;
 }
 
-int main() {
+void test_binary_search(string method, bool (*func)(int, int)) {
     vector<int> test_array {1, 2, 5, 5, 7, 10, 22};
     cout << "array = 1, 2, 5, 5, 7, 10, 22" << endl;
-    vector<pair<string, bool (*)(int, int)> > methods {
-        make_pair("binary_search", [](int v, int k) { return v > k; }),
-        make_pair("lower_bound", [](int v, int k) { return v == k; }),
-        make_pair("upper_bound", [](int v, int k) { return v >= k; }),
-    };
-    for (auto p: methods) {
-        cout << endl;
-        for (int key : {5, 6, 7, 10, 12}) {
-            cout << p.first  << " for " << key << " is index = "
-                 << binary_search(test_array, key, p.second) << endl;
-        }
-    }
+    for (int key : {5, 6, 7, 10, 12})
+        cout << method  << " for " << key << " is index = "
+             << binary_search(test_array, key, func) << endl;
+}
+
+int main() {
+    test_binary_search("binary_search", [](int v, int k) { return v > k; });
+    test_binary_search("lower_bound", [](int v, int k) { return v == k; });
+    test_binary_search("upper_bound", [](int v, int k) { return v >= k; });
 }
